c63e6bb1c05ff09752a11116f23ac9c5cd37516b
gigo <abedini.hassan.dc@gmail.com>
2026-09-18 16:58:04 +0330

    complete ui btn top

--- wp-content/themes/gp-dentashop/template-parts/footer/top.php
+++ wp-content/themes/gp-dentashop/template-parts/footer/top.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Footer - Top Bar (لوگو + بازگشت به بالا)
  */
@@ -9,29 +10,27 @@ if (!defined('ABSPATH')) exit;
 <div class="gpds-footer-top">
     <div class="gpds-container">
         <div class="gpds-footer-top__inner">
-            
+
             <!-- لوگو -->
             <div class="gpds-footer-top__logo">
                 <?php gpds_site_logo(['height' => 40]); ?>
             </div>
-            
+
             <!-- تلفن پشتیبانی -->
             <div class="gpds-footer-top__support">
                 <?php gpds_icon('phone', 20); ?>
-                <span>پشتیبانی ۲۴ ساعته: <strong>۰۲۱-۹۱۰۰۰۰۰۰</strong></span>
+                <span>پشتیبانی ۲۴ ساعته: <strong>056-0000000</strong></span>
             </div>
-            
+
             <!-- بازگشت به بالا -->
-            <button 
-                type="button" 
-                class="gpds-footer-top__back-to-top"
+            <button
+                type="button"
+                class="gpds-back-to-top"
                 data-gpds-back-to-top
-                aria-label="بازگشت به بالا"
-            >
-                <?php gpds_icon('arrow-up', 20); ?>
-                <span>بازگشت به بالا</span>
+                aria-label="بازگشت به بالا">
+                <?php gpds_icon('arrow-up', 22); ?>
             </button>
-            
+
         </div>
     </div>
 </div>
