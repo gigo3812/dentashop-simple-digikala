640d21d451e6bf8e5feed5167dab377a7fee7377
gigo <abedini.hassan.dc@gmail.com>
2026-09-19 13:32:19 +0330

    complete add url feather

--- wp-content/themes/gp-dentashop/inc/modules/features.php
+++ wp-content/themes/gp-dentashop/inc/modules/features.php
@@ -1,56 +1,57 @@
 <?php
+
 /**
- * Features Module - آیکون‌های سرویس
+ * Get Home Features
  *
  * @package GP_DentaShop
  */
 
 if (!defined('ABSPATH')) exit;
 
-/**
- * لیست آیکون‌های سرویس پیش‌فرض
- */
-function gpds_get_features() {
-    return apply_filters('gpds_features', [
-        [
-            'icon'  => 'rocket',
-            'title' => 'ارسال سریع',
-            'url'   => '#',
-        ],
-        [
-            'icon'  => 'shield',
-            'title' => 'ضمانت اصالت',
-            'url'   => '#',
-        ],
-        [
-            'icon'  => 'refresh',
-            'title' => 'بازگشت ۷ روزه',
-            'url'   => '#',
-        ],
-        [
-            'icon'  => 'headphones',
-            'title' => 'پشتیبانی ۲۴/۷',
-            'url'   => '#',
-        ],
-        [
-            'icon'  => 'credit-card',
-            'title' => 'خرید اقساطی',
-            'url'   => '#',
-        ],
-        [
-            'icon'  => 'location',
-            'title' => 'پرداخت در محل',
-            'url'   => '#',
-        ],
-        [
-            'icon'  => 'percent',
-            'title' => 'تخفیف ویژه',
-            'url'   => '#',
-        ],
-        [
-            'icon'  => 'star',
-            'title' => 'ویژه اعضا',
-            'url'   => '#',
-        ],
-    ]);
+if (!function_exists('gpds_get_features')) {
+    function gpds_get_features()
+    {
+        return [
+            [
+                'icon'  => 'rocket',
+                'title' => 'ارسال سریع',
+                'url'   => home_url('/fast-shipping'),
+            ],
+            [
+                'icon'  => 'shield',
+                'title' => 'ضمانت اصالت',
+                'url'   => home_url('/authenticity-guarantee'),
+            ],
+            [
+                'icon'  => 'refresh',
+                'title' => 'بازگشت ۷ روزه',
+                'url'   => home_url('/returns'),
+            ],
+            [
+                'icon'  => 'headphones',
+                'title' => 'پشتیبانی ۲۴/۷',
+                'url'   => home_url('/support'),
+            ],
+            [
+                'icon'  => 'credit-card',
+                'title' => 'خرید اقساطی',
+                'url'   => home_url('/installment-payment'),
+            ],
+            [
+                'icon'  => 'location',
+                'title' => 'پرداخت در محل',
+                'url'   => home_url('/cash-on-delivery'),
+            ],
+            [
+                'icon'  => 'percent',
+                'title' => 'تخفیف ویژه',
+                'url'   => home_url('/special-offers'),
+            ],
+            [
+                'icon'  => 'star',
+                'title' => 'ویژه اعضا',
+                'url'   => home_url('/membership'),
+            ],
+        ];
+    }
 }
